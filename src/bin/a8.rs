c1bf045a3b17f72ad6ffee9c66118b96fdce3f5a
Sushant Suwal <sushantdsuwal@gmail.com>
2021-12-08 16:35:32 +0545

    Organizing similar data using structs

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,40 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavor {
+  Sparkling,
+  Sweet,
+  Fruity,
+}
+
+struct Drink {
+  flavor: Flavor,
+  fluid_oz: f64,
+}
+
+fn print_drink(drink: Drink){
+  match drink.flavor {
+    Flavor::Sparkling => println!("Flavor: Sparkling"),
+    Flavor::Sweet => println!("Flavor: Sweet"),
+    Flavor::Fruity => println!("Flavor: Fruity"),
+  };
+  println!("oz {:?}", drink.fluid_oz);
+}
+
+fn main() {
+  let sweet = Drink{
+    flavor: Flavor::Sweet,
+    fluid_oz: 5.0,
+  };
+  print_drink(sweet);
+  let furity = Drink{
+    flavor: Flavor::Fruity,
+    fluid_oz: 5.0,
+  };
+  print_drink(furity);
+  let sparkling = Drink{
+    flavor: Flavor::Sparkling,
+    fluid_oz: 15.0,
+  };
+  print_drink(sparkling);
+}
